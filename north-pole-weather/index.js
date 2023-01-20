7fd2c07537b25f57fe1aa8e72a4f42d5729fe3ac
Hiroko Matsuyama <110796421+hmatsuyama3@users.noreply.github.com>
2023-01-20 00:08:08 -0500

    Added more descriptors
    for fun :smile:

--- north-pole-weather/index.js
+++ north-pole-weather/index.js
@@ -1,9 +1,10 @@
 const latitude = 90;
 const longitude = 135;
-const key = "d7feb51c973b7fcdf75a94a692bbacc7";
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=90&lon=135&appid=36dbddb8b9ad7bed185fb998d419b35a";
 
 let temperature = document.querySelector(".temp");
+let description = document.querySelector(".description");
+let feelslike = document.querySelector(".feelslike");
 
 window.addEventListener("load", ()=>{
 fetch(url)
@@ -13,7 +14,10 @@ fetch(url)
     .then((data)=>{
         console.log(data);
         temperature.textContent="The current temp is " +
-            Math.floor(data.main.temp - 273) + "\u00B0C";
-        console.log(data.main.temp);
+            Math.floor(data.main.temp - 273) + "\u00B0C.";
+        feelslike.textContent="It feels like " + 
+            Math.floor(data.main.feels_like - 273) + "\u00B0C.";
+        description.textContent="If you looked outside, you'd see " +
+            data.weather[0].description + ".";
     })
 })
