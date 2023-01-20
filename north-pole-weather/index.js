8f1772b22fc08f80210e104c5df5d24c04a248dd
Hiroko Matsuyama <110796421+hmatsuyama3@users.noreply.github.com>
2023-01-20 00:00:57 -0500

    Wait I got it to work actually
    after rechecking the documentation & realizing I had the wrong url. I had tried several because my key was giving me a different 401 error, but maybe my key just wasn't ready yet?

--- north-pole-weather/index.js
+++ north-pole-weather/index.js
@@ -1,7 +1,7 @@
 const latitude = 90;
 const longitude = 135;
 const key = "d7feb51c973b7fcdf75a94a692bbacc7";
-const url = "https://api.openweathermap.org/data/3.0/onecall?lat=90&lon=135&exclude={part}&appid=d7feb51c973b7fcdf75a94a692bbacc7";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=90&lon=135&appid=36dbddb8b9ad7bed185fb998d419b35a";
 
 let temperature = document.querySelector(".temp");
 
@@ -12,8 +12,8 @@ fetch(url)
     })
     .then((data)=>{
         console.log(data);
-        temperature.textContent=
-            Math.floor(data.main.temp - 273) + "C";
+        temperature.textContent="The current temp is " +
+            Math.floor(data.main.temp - 273) + "\u00B0C";
         console.log(data.main.temp);
     })
 })
